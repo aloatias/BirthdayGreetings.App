bbb9624207614a251df6c713da98e1bbc8eeecd3
Alvaro Atias <aat@softfluent.com>
2020-03-04 23:44:05 +0100

    Database DI

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using BirthdayGreetings.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
 
 namespace BirthdayGreetings
 {
     class Program
     {
+        private static IConfiguration _configuration;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BuildConfiguration();
+            InjectDependencies();
+        }
+
+        private static void BuildConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(AppContext.BaseDirectory))
+                .AddJsonFile("appsettings.json", false, true);
+
+            _configuration = builder.Build();
+        }
+
+        private static void InjectDependencies()
+        {
+            new ServiceCollection()
+                .AddDbContext<Context>(options => options.UseSqlServer(_configuration.GetConnectionString("BirthDayGreetingsConnection")));
         }
     }
 }
